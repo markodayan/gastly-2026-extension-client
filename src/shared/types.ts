8b7a935bb3cfd262bd469f8bfe4a2121e1674164
Mark Odayan <markodayansa@gmail.com>
2026-04-07 19:20:04 +0200

    initial commit

---
+++ src/shared/types.ts
@@ -0,0 +1,79 @@
+export type GasSpeed = 'fast' | 'average' | 'slow';
+export type FiatPreference = 'ethusd' | 'ethzar' | 'etheur' | 'ethaud';
+export type TransactionPreference = 'eth-send';
+
+/**
+ * Legacy API block type
+ */
+export type BlockData = {
+  gasLimit: number;
+  gasUsed: number;
+  nonce: number;
+  number: number;
+  size: number;
+  timestamp: Date;
+  uncles: string[];
+  gasUtilizationRatio: number;
+  txCount: number;
+  basefee: number;
+  priorityFees: {
+    fast: number;
+    average: number;
+    slow: number;
+  };
+};
+
+/**
+ * Legacy API spot data type
+ */
+export type SpotData = {
+  [ticker: string]: string;
+};
+
+/**
+ * Updated API block type
+ */
+// export type BlockData = {
+//   gasLimit: number;
+//   gasUsed: number;
+//   nonce: number;
+//   number: number;
+//   size: number;
+//   timestamp: Date;
+//   uncles: string[];
+//   gasUtilizationRatio: number;
+//   txCount: number;
+//   basefee: number;
+//   priorityFees: {
+//     fast: number;
+//     average: number;
+//     slow: number;
+//   };
+// };
+
+/**
+ * Updated API spot data type
+ */
+// export type SpotData = {
+//   [ticker: string]: number;
+// }
+
+export type Preferences = {
+  gasPreference: GasSpeed;
+  fiatPreference: FiatPreference;
+  transactionPrefences: TransactionPreference;
+};
+
+export type ConnectionState = {
+  wsConnected: boolean;
+  internetReachable: boolean;
+  lastSpotFetchAt?: number;
+  lastBlockAt?: number;
+};
+
+export type ExtensionState = {
+  block?: BlockData;
+  spots?: SpotData;
+  preferences: Preferences;
+  connection: ConnectionState;
+};
